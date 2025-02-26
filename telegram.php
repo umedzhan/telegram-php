f334a80b71a5e69df47f9617e75061c5ff881ef5
umedzhan <umedzhan@kali.kali>
2025-02-26 12:30:00 +0500

    update

--- telegram.php
+++ telegram.php
@@ -46,17 +46,85 @@ class Telegram {
 		return $this->getUpdate()[$this->Message()]['chat']['last_name'] ?? NULL;
 	}
 	
-	
-	
 	public function Text() {
 		$update = $this->getUpdate();
 		return isset($update[$this->Message()]['text']) ? $update['message']['text'] : '';
 	}
 	
-	public function sendMessage($chat_id, $text) {
+	public function sendMessage($chat_id, $text, $parse_mode = '') {
 		return $this->bot('sendMessage', [
 			'chat_id' => $chat_id,
-			'text' => $text
+			'text' => $text,
+			'parse_mode' => $parse_mode
+		]);
+	}
+
+	public function sendPhoto($chat_id, $photo, $caption = '') {
+		return $this->bot('sendPhoto', [
+			'chat_id' => $chat_id,
+			'photo' => $photo,
+			'caption' => $caption
+		]);
+	}
+
+	public function sendVideo($chat_id, $video, $caption = '') {
+		return $this->bot('sendVideo', [
+			'chat_id' => $chat_id,
+			'video' => $video,
+			'caption' => $caption
+		]);
+	}
+
+	public function sendAudio($chat_id, $audio, $caption = '') {
+		return $this->bot('sendAudio', [
+			'chat_id' => $chat_id,
+			'audio' => $audio,
+			'caption' => $caption
+		]);
+	}
+
+	public function sendDocument($chat_id, $document, $caption = '') {
+		return $this->bot('sendDocument', [
+			'chat_id' => $chat_id,
+			'document' => $document,
+			'caption' => $caption
+		]);
+	}
+
+	public function sendSticker($chat_id, $sticker) {
+		return $this->bot('sendSticker', [
+			'chat_id' => $chat_id,
+			'sticker' => $sticker
+		]);
+	}
+
+	public function sendAnimation($chat_id, $animation, $caption = '') {
+		return $this->bot('sendAnimation', [
+			'chat_id' => $chat_id,
+			'animation' => $animation,
+			'caption' => $caption
+		]);
+	}
+
+	public function sendVoice($chat_id, $voice) {
+		return $this->bot('sendVoice', [
+			'chat_id' => $chat_id,
+			'voice' => $voice
+		]);
+	}
+
+	public function sendLocation($chat_id, $latitude, $longitude) {
+		return $this->bot('sendLocation', [
+			'chat_id' => $chat_id,
+			'latitude' => $latitude,
+			'longitude' => $longitude
+		]);
+	}
+
+	public function sendChatAction($chat_id, $action) {
+		return $this->bot('sendChatAction', [
+			'chat_id' => $chat_id,
+			'action' => $action
 		]);
 	}
 }
